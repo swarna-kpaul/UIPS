795c741c2ec399a540f463c28e871912b85bce04
swarna-kpaul <swarna.kpaul@gmail.com>
2019-03-19 11:07:46 -0400

    Add files via upload

--- graph_view.r
+++ graph_view.r
@@ -1,7 +1,7 @@
 library(visNetwork)
 library(igraph)
-edges<-unique(read.csv('C:/skp/phd/UIPS/edge_list.csv'))
-nodes<-unique(read.csv('C:/skp/phd/UIPS/node_list.csv'))
+edges<-unique(read.csv('edge_list.csv'))
+nodes<-unique(read.csv('node_list.csv'))
 #nodes <- data.frame(id=unique(c(as.character(edges$from),as.character(edges$to))))
 nodes$label = gsub("[0-9]*$","",nodes$id)
 nodes$group = nodes$label
@@ -10,4 +10,4 @@ nodes$shape = 'circle'
 net<-visNetwork(nodes, edges, height = "700px", width = "100%") %>% visEdges(arrows ="to") %>% 
 visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%visLegend()  %>% visInteraction(navigationButtons = TRUE) %>% visIgraphLayout()
 
-visSave(net,"C:/skp/phd/UIPS/graph_view.html",selfcontained = FALSE)
+visSave(net,"graph_view.html",selfcontained = FALSE)
